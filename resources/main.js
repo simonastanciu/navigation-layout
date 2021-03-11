35c9d1e8182b3dbe5c36e7019cde509bdb48ff67
Simona Stanciu <simona.stanciu92@gmail.com>
2021-03-11 21:29:42 +0200

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -1,64 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     const menuItems = document.getElementsByClassName('menu-item');
-//     console.log('menu-item', menuItems)
-
-//     for (let x = 0; x < menuItems.length; x++) {
-//         const menuItem = menuItems[x];
-
-//         menuItem.addEventListener('click', function() {
-//             for (let y = 0; y < menuItems.length; y++) {
-//                 if(menuItems[y].classList.contains('selected-item')) {
-//                     menuItems[y].classList.remove('selected-item');
-//                 } else {
-//                     continue;
-//                 }
-//             }
-//             menuItem.classList.add('selected-item');  
-
-//             const dataContent = menuItem.dataset.content;
-//             const pageContent = document.getElementsByClassName('content');
-//             for(let z = 0; z < pageContent.length; z++) {
-//                 if(pageContent[z].classList.contains('hidden')) {
-//                     continue;
-//                 } else {
-//                     pageContent[z].classList.add('hidden');
-//                 }
-//             }    
-//             document.getElementsByClassName(dataContent)[0].classList.remove('hidden');
-//             console.log('show content', dataContent);
-//         });
-//     }
-// });
-
 $(function () {
     const menuItem = $('.menu-item');
-    // la ce facem aici console.log? la text? intre ghilimele ai sa vezi exact textul, daca vrei sa iti verifici selectia, folosesti numele constantei
-    console.log('menu-item');
     const pageContent = $('.content');
-    console.log('page-content');
-
-    menuItem.click(function(e) {
-        // ce verificam aici? nu imi e clar
-        if(!$(this).data('content')) {
-            pageContent.addClass('hidden');               
-        } 
-
-        // esti pe menuItem, aici nu vrei sa ascunzi menuItem, corect?
-        $(this).removeClass('hidden');
+    const yourOrders = $('ul').find('li:nth-child(1)');
+    
+    ////////////////////////////////////
+    menuItem.click(function() {
+        pageContent.addClass('hidden');
+        $("." + $(this).data('content')).removeClass('hidden');
+        // $(`.${$(this).data('content')}`).removeClass('hidden');
         menuItem.removeClass('selected-item');
         $(this).addClass('selected-item');
-
         $('.show-header').show();
     });
-    console.log($('.menu-item').data('content'));
-
-
-    //////////////////////////////////
-    menuItem.click(function() {
-        $('.content').addClass('hidden');
-        $(`.${$(this).data('content')}`).removeClass('hidden');
-    });
     ////////////////////////////////////
+
+    yourOrders.click(function() {
+        $('.show-header').hide();
+    });
 
     $('#orderDetailsBtn').click(function () {
         $('#orderDetails').removeClass('hidden');
@@ -66,8 +24,6 @@ $(function () {
     });
     console.log ('order-link', $('#orderDetailsBtn'));
 
-
     $('#saveButton').on('click', function() {
-        console.log ('button-click');
     });
 })
